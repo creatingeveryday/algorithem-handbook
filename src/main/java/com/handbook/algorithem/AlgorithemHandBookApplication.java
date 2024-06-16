f23d04e944a75f13b90f538e82fed4f3f7b5ab2f
JongKuk Seo <jongkuk.dev@gmail.com>
2024-06-16 20:24:30 +0900

    init

---
+++ src/main/java/com/handbook/algorithem/AlgorithemHandBookApplication.java
@@ -0,0 +1,13 @@
+package com.handbook.algorithem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithemHandBookApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlgorithemHandBookApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/handbook/algorithem/AlgorithemHandBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handbook.algorithem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithemHandBookApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
